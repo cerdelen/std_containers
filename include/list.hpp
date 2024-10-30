e3b829953a8a56193df3451cb002d11ef8c9bb0c
cerdelen <cedric.erdelen@gmail.com>
2024-10-30 11:01:04 +0100

    size()

--- include/list.hpp
+++ include/list.hpp
@@ -11,12 +11,6 @@ namespace ft {
 
     template<typename T, typename Alloc = std::allocator<T> >
     class list {
-        private:
-            struct Node {
-                Node* next;
-                Node* prev;
-                T*      val;
-            };
         public:
             typedef				T										value_type;
 			typedef				Alloc									allocator_type;
@@ -26,17 +20,90 @@ namespace ft {
 			typedef typename	allocator_type::const_pointer			const_pointer;
 			typedef typename	allocator_type::size_type				size_type;
 
-            list(){};
-            list(const list&) {};
-            list& operator=(const list&) ;
-            list(list&&) {};
-            list& operator=(list&&) ;
+            list(): _size(0), _begin(nullptr), _end(nullptr) {};
+            list(const list& other): _size(other._size){};
+            list& operator=(const list& other) ;
+            list(list&& other) {};
+            list& operator=(list&& other) ;
             ~list() {};
 
+            size_type size() const noexcept {return _size;};
+
+            // Parameters:
+            //     value: the value of the element to prepend
+            // Return value:
+            //     (none)
+            // Complexity:
+            //     Constant
+            // Exceptions:
+            //     If an exception is thrown for any reason, these functions have no effect (strong exception safety guarantee).
+            void push_front(const T& value) {
+                Node* new_node = node_alloc.allocate(1);
+
+                new_node->val = value;
+                new_node->next = _begin;
+                new_node->prev = nullptr;
 
 
+                if (_begin == nullptr) {
+                    _end = new_node;
+                } else {
+                    _begin->prev = new_node;
+                }
+
+                _begin = new_node;
+                _size += 1;
+
+            };
+
+            // Parameters:
+            //     value: the value of the element to append
+            // Return value:
+            //     (none)
+            // Complexity:
+            //     Constant
+            // Exceptions:
+            //     If an exception is thrown for any reason, these functions have no effect (strong exception safety guarantee).
+            void push_back(const T& value) {
+                Node* new_node = node_alloc.allocate(1);
+
+                new_node->val = value;
+                new_node->prev = _end;
+                new_node->next = nullptr;
 
 
-            size_type size() const {return 0;};
+                if (_begin == nullptr) {
+                    _begin = new_node;
+                } else {
+                    _end->next = new_node;
+                }
+
+                _end = new_node;
+                _size += 1;
+            };
+
+            // Parameters
+            //     (none)
+            // Return value
+            //     Reference to the first element.
+            // Complexity
+            //     Constant.
+            // Notes
+            //     For a container c, the expression c.front() is equivalent to *c.begin().
+            reference front() {*(this->begin())};
+            const_reference front() const {};
+
+
+        private:
+            struct Node {
+                Node* next;
+                Node* prev;
+                T      val;
+            };
+            size_type   _size;
+            Node*   _begin;
+            Node*   _end;
+            std::allocator<Node> node_alloc;
+            allocator_type _alloc;
     };
 }
--- test/list/test_constructor.cpp
+++ test/list/test_constructor.cpp
@@ -1,8 +1,67 @@
 #include <gtest/gtest.h>
+#include <list>
 #include <list.hpp>
 
-TEST(HelloTestm, BasicAssertions) {
-    ft::list<int> a;
-    EXPECT_EQ(0, a.size());
+TEST(TestingList, Constructing) {
+    ft::list<int> mine;
+}
+
+TEST(TestingList, Constructing_0_size) {
+    ft::list<int> mine;
+    std::list<int> orig;
+    EXPECT_EQ(orig.size(), mine.size());
+}
+
+TEST(TestingList, Simple_push_front) {
+    ft::list<int> mine;
+    std::list<int> orig;
+
+    mine.push_front(1);
+    orig.push_front(1);
+    EXPECT_EQ(orig.size(), mine.size());
+}
+
+TEST(TestingList, Multiple_push_front_size) {
+    ft::list<int> mine;
+    std::list<int> orig;
+
+    mine.push_front(1);
+    orig.push_front(1);
+
+    mine.push_front(2);
+    orig.push_front(2);
+
+    mine.push_front(3);
+    orig.push_front(3);
+
+    mine.push_front(4);
+    orig.push_front(4);
+    EXPECT_EQ(orig.size(), mine.size());
+}
+
+TEST(TestingList, Multiple_push_front_size) {
+    ft::list<int> mine;
+    std::list<int> orig;
+
+    mine.push_front(1);
+    orig.push_front(1);
+
+    EXPECT_EQ(orig.front(), mine.front());
+    EXPECT_EQ(orig.back(), mine.back());
+    mine.push_front(2);
+    orig.push_front(2);
+
+    EXPECT_EQ(orig.front(), mine.front());
+    EXPECT_EQ(orig.back(), mine.back());
+    mine.push_front(3);
+    orig.push_front(3);
+
+    EXPECT_EQ(orig.front(), mine.front());
+    EXPECT_EQ(orig.back(), mine.back());
+    mine.push_front(4);
+    orig.push_front(4);
+
+    EXPECT_EQ(orig.front(), mine.front());
+    EXPECT_EQ(orig.back(), mine.back());
 }
 
