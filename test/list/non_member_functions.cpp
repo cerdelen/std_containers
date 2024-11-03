f59fdd17798f39981416d9eed363ab0be7d99817
cerdelen <cedric.erdelen@gmail.com>
2024-11-03 08:39:04 +0100

    added few comments to testing comparison operators and fixed a few typos

--- test/list/non_member_functions.cpp
+++ test/list/non_member_functions.cpp
@@ -5,45 +5,61 @@ class ListNonMemberFunctionsTests : public ListTestBaseFixture { };
 TEST_F(ListNonMemberFunctionsTests, equality_operator) {
     ft::list<int> mine1;
     std::list<int> orig1;
+
+    // empty, equal in size and content
+    EXPECT_EQ((mine1 == mine), (orig1 == orig));
+    EXPECT_EQ((mine1 == mine), true);
+
+
     mine.push_back(5);
     orig.push_back(5);
-    EXPECT_EQ((mine1 != mine), (orig1 != orig));
+    // unequal in size
+    EXPECT_EQ((mine1 == mine), (orig1 == orig));
+    EXPECT_EQ((mine1 == mine), false);
 
     mine1.push_back(5);
     orig1.push_back(5);
-    EXPECT_EQ((mine1 != mine), (orig1 != orig));
-
-    push_back(1);
+    // equal in size and content
     EXPECT_EQ((mine1 == mine), (orig1 == orig));
+    EXPECT_EQ((mine1 == mine), true);
 
-    push_front(2);
-    EXPECT_EQ((mine1 == mine), (orig1 == orig));
-
+    mine1.push_back(5);
+    orig1.push_back(5);
     mine.push_back(99);
     orig.push_back(99);
+    //equal in size, unequal in content
     EXPECT_EQ((mine1 == mine), (orig1 == orig));
+    EXPECT_EQ((mine1 == mine), false);
 }
 
 TEST_F(ListNonMemberFunctionsTests, inequality_operator) {
     ft::list<int> mine1;
     std::list<int> orig1;
+
+    // empty, equal in size and content
+    EXPECT_EQ((mine1 != mine), (orig1 != orig));
+    EXPECT_EQ((mine1 != mine), false);
+
+
     mine.push_back(5);
     orig.push_back(5);
+    // unequal in size
     EXPECT_EQ((mine1 != mine), (orig1 != orig));
+    EXPECT_EQ((mine1 != mine), true);
 
     mine1.push_back(5);
     orig1.push_back(5);
+    // equal in size and content
     EXPECT_EQ((mine1 != mine), (orig1 != orig));
+    EXPECT_EQ((mine1 != mine), false);
 
-    push_back(1);
-    EXPECT_EQ((mine1 != mine), (orig1 != orig));
-
-    push_front(2);
-    EXPECT_EQ((mine1 != mine), (orig1 != orig));
-
+    mine1.push_back(5);
+    orig1.push_back(5);
     mine.push_back(99);
     orig.push_back(99);
+    //equal in size, unequal in content
     EXPECT_EQ((mine1 != mine), (orig1 != orig));
+    EXPECT_EQ((mine1 != mine), true);
 }
 
 // for greater/smaller operations this should be proficient.
@@ -67,6 +83,10 @@ TEST_F(ListNonMemberFunctionsTests, inequality_operator) {
 TEST_F(ListNonMemberFunctionsTests, less_than_operator) {
     ft::list<int>   mine1(mine);
     std::list<int>  orig1(orig);
+    EXPECT_EQ((mine < mine1), (orig < orig1));
+    EXPECT_EQ((mine < mine1), false);
+    EXPECT_EQ((mine1 < mine), (orig1 < orig));
+    EXPECT_EQ((mine1 < mine), false);
 
     push_back(1);
     mine1.push_back(1);
@@ -93,6 +113,10 @@ TEST_F(ListNonMemberFunctionsTests, less_than_operator) {
 TEST_F(ListNonMemberFunctionsTests, greater_than_operator) {
     ft::list<int>   mine1(mine);
     std::list<int>  orig1(orig);
+    EXPECT_EQ((mine > mine1), (orig > orig1));
+    EXPECT_EQ((mine > mine1), false);
+    EXPECT_EQ((mine1 > mine), (orig1 > orig));
+    EXPECT_EQ((mine1 > mine), false);
 
     push_back(1);
     mine1.push_back(1);
@@ -119,6 +143,10 @@ TEST_F(ListNonMemberFunctionsTests, greater_than_operator) {
 TEST_F(ListNonMemberFunctionsTests, less_than_or_equal_operator) {
     ft::list<int>   mine1(mine);
     std::list<int>  orig1(orig);
+    EXPECT_EQ((mine <= mine1), (orig <= orig1));
+    EXPECT_EQ((mine <= mine1), true);
+    EXPECT_EQ((mine1 <= mine), (orig1 <= orig));
+    EXPECT_EQ((mine1 <= mine), true);
 
     push_back(1);
     mine1.push_back(1);
@@ -146,6 +174,10 @@ TEST_F(ListNonMemberFunctionsTests, less_than_or_equal_operator) {
 TEST_F(ListNonMemberFunctionsTests, greater_than_or_equal_operator) {
     ft::list<int>   mine1(mine);
     std::list<int>  orig1(orig);
+    EXPECT_EQ((mine >=  mine1), (orig >=  orig1));
+    EXPECT_EQ((mine >=  mine1), true);
+    EXPECT_EQ((mine1 >=  mine), (orig1 >=  orig));
+    EXPECT_EQ((mine1 >=  mine), true);
 
     push_back(1);
     mine1.push_back(1);
